b81c6292de2575a17361de3c325f252ceedd35bb
renzotev <renzotelenta@gmail.com>
2014-01-24 18:31:28 -0500

    Tarea Node JS

---
+++ server.js
@@ -0,0 +1,54 @@
+var express = require("express"),
+	swig = require("swig"),
+	cons = require("consolidate");
+
+var app = express();
+
+swig.setDefaults({
+	cache : false
+});
+
+
+// View engine
+app.engine(".html", cons.swig);
+app.set("view engine", "html");
+app.set("views", "./views");
+
+// Static files
+app.use(express.static("./public"));
+
+// Post
+//app.use( express.bodyParser() );
+//app.use( express.cookieParser() );
+
+app.use(express.urlencoded());
+app.use(express.json());
+//app.use(express.multipart());
+
+
+var mensajes = [],
+	ress = [];
+
+app.get("/", function (req, res) {
+	res.render("home", {
+		mensajes : mensajes
+	});
+});
+
+app.post("/mensajes/new", function(req, res) {
+	mensajes.push(req.body.mensaje);
+
+	ress.forEach(function(res) {
+		res.send(mensajes);
+	});
+
+	res.send("Tu mensaje es "+ req.body.mensaje);
+});
+
+app.get("/mensajes/list", function(req, res){
+	ress.push(res);
+	//res.send(mensajes+"<script>setTimeout(function(){window.location.reload()},1000)</script>");
+});
+
+app.listen(3000);
+console.log("App up");
