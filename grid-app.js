9bce408c8fce68fbb3bc289ba6d85631e8f8f9ef
renzotev <renzotelenta@gmail.com>
2014-01-24 18:36:54 -0500

    socket io

--- grid-app.js
+++ grid-app.js
@@ -1,6 +1,19 @@
 var express = require("express");
-var app = (express());
+var app 	= (express());
+var server 	= require("http").createServer(app);
+var io 		= require("socket.io").listen(server);
 
-var server = require("http").createServer(app);
+var cons 	= require("consolidate");
 
-var io = require
+server.listen(3000);
+
+app.engine(".html", cons.jade);
+app.set("view engine", "html");
+
+app.use(express.static("./public"));
+
+app.get("/", function() {
+	res.render("index", {
+		titulo : "applicacion grip"
+	});
+});
